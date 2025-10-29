2dbc80894e2a375b111ae3a7030e9476291075ab
GracieleXavierDoNascimento <gxn@discente.ifpe.edu.br>
2025-10-28 23:26:57 -0300

    Removendo submódulos e adicionando serviços como pastas normais

---
+++ services/users-service/src/users/users.controller.ts
@@ -0,0 +1,24 @@
+// src/users/users.controller.ts
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { UsersService } from './user.service';
+import { User } from './user.entity';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() data: Partial<User>) {
+    return this.usersService.create(data);
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.usersService.findOne(id);
+  }
+}
